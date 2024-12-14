3611cfcc9887fb3c95aad29d6ce179592fe3987c
mahmoudessam16 <mahmoudemourad29@gmail.com>
2024-12-14 15:02:52 +0200

    Clear some data in console

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ let closeIcon = document.querySelector(".close");
 let mobileLinks = document.querySelector(".mobile-links");
 
 
-console.log(hamburgerIcon);
-console.log(closeIcon);
-console.log(mobileLinks);
-
 hamburgerIcon.addEventListener('click', function () {
     mobileLinks.style.left = "0%";
     closeIcon.addEventListener('click', function () {
